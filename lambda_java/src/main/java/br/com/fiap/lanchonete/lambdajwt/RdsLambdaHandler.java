7fb3aaef107ea4d430da027e4faa709e5502d945
Nicolas Silva <nicolasoliveirasilva98@gmail.com>
2025-05-22 21:58:37 -0300

    feature: rodrigo changes

--- lambda_java/src/main/java/br/com/fiap/lanchonete/lambdajwt/RdsLambdaHandler.java
+++ lambda_java/src/main/java/br/com/fiap/lanchonete/lambdajwt/RdsLambdaHandler.java
@@ -1,6 +1,7 @@
-package br.com.fiap.lanchonete.lambdajwt;
+package br.com.fiap.lanchonente.lambdajwt;
 
-import com.amazonaws.services.lambda.runtime.*;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.secretsmanager.AWSSecretsManager;
 import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
 import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
@@ -14,6 +15,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Date;
 import java.util.Map;
 
@@ -23,38 +25,20 @@ import java.util.Map;
 - Signs JWT using HMAC256 for cryptographic security.
 - Sets token expiration (1 hour) to enforce security best practices.
  */
-public class RdsLambdaHandler implements RequestHandler <String, String> {
-    private LambdaLogger logger = null;
+public class RdsLambdaHandler {
 
-    @Override
     public String handleRequest(String cpf, Context context) {
-        logger = context.getLogger();
+        LambdaLogger logger = context.getLogger();
         logger.log("Handler invoked.");
-
         String jwt = null;
-        String dbSchema = "lanch";
-
-        /*String dbHost = "lanchonete-db.cijutajfn35x.us-east-1.rds.amazonaws.com:5432";
+        String dbHost = "my-database-1.cyhpsurw38ch.us-east-1.rds.amazonaws.com";
         String dbName = "lanchdb";
         String dbPort = "5432";
+        String dbSchema = "lanch";
         String dbUser = "lanchuser";
-        String dbPassword = "lanch1234";*/
-
+        String dbPassword = "lanch01234!#";
+        String dbUrl = "jdbc:postgresql://" + dbHost + ":" + dbPort + "/" + dbName;
         Connection conn = null;
-
-
-        String dbHost = System.getenv("DB_HOST") != null ? System.getenv("DB_HOST") : "your-rds-endpoint.amazonaws.com";
-        String dbName = System.getenv("DB_NAME") != null ? System.getenv("DB_NAME") : "your_database_name";
-        String dbUser = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "your_database_user";
-        String dbPassword = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "your_database_password";
-        //public static final String EXTERNAL_IP_API = System.getenv("EXTERNAL_IP_API") != null ? System.getenv("EXTERNAL_IP_API") : "default-elb";
-        //public static final String EXTERNAL_IP_PAYMENT = System.getenv("EXTERNAL_IP_PAYMENT") != null ? System.getenv("EXTERNAL_IP_PAYMENT") : "default-elb";
-
-        String dbUrl = "jdbc:postgresql://" + dbHost + "/" + dbName+"?useSSL=true&requireSSL=true";
-
-        logger.log("Host: "+dbHost+", Name: "+dbName+", URL: "+dbUrl+", Password: "+dbPassword);
-
-
         try {
             /*
             // Retrieve credentials from AWS Secrets Manager
@@ -64,54 +48,42 @@ public class RdsLambdaHandler implements RequestHandler <String, String> {
 */
             // Establish connection to RDS
             conn = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
-            logger.log("Antes de verificar conexão: "+conn);
             if (!conn.isValid(0)) {
                 logger.log("Failed to connect to RDS: " + dbUrl);
                 return jwt;
             }
 
-            //this.checkIfSchemaExists(conn, dbSchema, logger);
+            this.checkIfSchemaExists(conn, dbSchema, logger);
 
             String query = "SELECT nr_cpf FROM " + dbSchema + ".tb_cliente WHERE nr_cpf = ?";
-            logger.log("SQL query :"+query);
             try (PreparedStatement stmt = conn.prepareStatement(query)) {
                 stmt.setString(1, cpf);
                 ResultSet rs = stmt.executeQuery();
                 if (rs.next()) {
                     logger.log("CPF found.");
                     jwt = generateJwt(cpf);
-                    logger.log("JWT CPF found: "+jwt);
                 } else {
                     logger.log("CPF NOT found.");
                     jwt = generateJwt("");
-                    logger.log("JWT CPF NOT found: "+jwt);
                 }
             }
         } catch (SQLException e) {
-            logger.log("Catch SQLException.");
             throw new RuntimeException(e);
         } catch (JsonProcessingException e) {
-            logger.log("Catch JsonProcessingException.");
             throw new RuntimeException(e);
         } finally {
-            logger.log("Entrou no finally: "+conn);
             if (conn != null) {
                 try {
                     conn.close();
-                    logger.log("Finally.");
                 } catch (SQLException e) {
-                    logger.log("Catch Finally.");
                     throw new RuntimeException(e);
                 }
-            }else {
-                logger.log("Else do finally: "+conn);
             }
         }
-        logger.log("JWT: "+jwt);
         return jwt;
     }
 
-    /*private void checkIfSchemaExists(Connection conn, String dbSchema, LambdaLogger logger) {
+    private void checkIfSchemaExists(Connection conn, String dbSchema, LambdaLogger logger) {
         logger.log("Checking if schema exists...");
         String createSchemaStmt = "CREATE SCHEMA IF NOT EXISTS " + dbSchema + " AUTHORIZATION lanchuser";
         Statement statement = null;
@@ -144,34 +116,24 @@ public class RdsLambdaHandler implements RequestHandler <String, String> {
         // Parse the JSON response containing credentials
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.readValue(result.getSecretString(), Map.class);
-    }*/
+    }
 
     private String generateJwt(String cpf) throws JsonProcessingException {
-        logger.log("Entrou no método de gerar JWT.");
-        try {
-            return JWT.create()
-                    .withIssuer("LanchoneteApp")
-                    .withSubject("UserAuthentication")
-                    .withClaim("cpf", cpf)
-                    .withExpiresAt(new Date(System.currentTimeMillis() + 3600 * 1000)) // 1-hour expiration
-                    .sign(Algorithm.HMAC256(this.getJwtSecret()));
-        }catch (Exception e){
-            logger.log("Erro ao gerar JWT: "+e);
-            return null;
-        }
+        return JWT.create()
+                .withIssuer("LanchoneteApp")
+                .withSubject("UserAuthentication")
+                .withClaim("cpf", cpf)
+                .withExpiresAt(new Date(System.currentTimeMillis() + 3600 * 1000)) // 1-hour expiration
+                .sign(Algorithm.HMAC256(this.getJwtSecret()));
     }
 
     private String getJwtSecret() throws JsonProcessingException {
-        logger.log("Entrou no método de get JWT Secret.");
-        String jwtSecretName = System.getenv("JWT_SECRET_NAME") != null ? System.getenv("JWT_SECRET_NAME") : "name-jwt-secret";
-        logger.log("JWT Secret Name: "+jwtSecretName);
         AWSSecretsManager client = AWSSecretsManagerClientBuilder.standard().build();
-        GetSecretValueRequest request = new GetSecretValueRequest().withSecretId(jwtSecretName);
+        GetSecretValueRequest request = new GetSecretValueRequest().withSecretId("jwt-secret-key");
         GetSecretValueResult result = client.getSecretValue(request);
-        logger.log("Antes de chamar o ObjectMapper de get JWT Secret.");
+
         ObjectMapper objectMapper = new ObjectMapper();
         Map<String, String> secretMap = objectMapper.readValue(result.getSecretString(), Map.class);
-        logger.log("Ultima linha do método de get JWT Secret.");
         return secretMap.get("jwt-key");
     }
 }
